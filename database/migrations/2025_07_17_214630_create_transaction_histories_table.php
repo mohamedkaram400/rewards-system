b67cbaa0bd990f1f51c30e99f210afd8b8688c1d
Mohamed Karam <mohamedkaramm500@gmail.com>
2025-07-18 01:16:29 +0300

    Add transaction history model and migration

---
+++ database/migrations/2025_07_17_214630_create_transaction_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_histories', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount');
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('type', ['redeem', 'earn', 'transfer_out', 'transfer_in', 'bonus', 'purchase_reward']);
+            $table->string('source');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_histories');
+    }
+};
