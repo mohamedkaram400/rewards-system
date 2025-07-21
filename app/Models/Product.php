51b90df943dfd59090c720aff9d206741a12227f
Mohamed Karam <mohamedkaramm500@gmail.com>
2025-07-21 21:28:57 +0300

    Add/Configer elastic search with dokcer

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,37 @@
 
 namespace App\Models;
 
+use Elastic\ScoutDriverPlus\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
+    use Searchable;
+
     protected $fillable = ['name', 'description', 'price', 'point_cost', 'is_offer_pool', 'category_id'];
+
+    public function searchableAs(): string
+    {
+         return env('SCOUT_PREFIX') .'_products';
+    }
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'point_cost' => $this->point_cost,
+            'is_offer_pool' => $this->is_offer_pool,
+            'category_id' => $this->category_id,
+        ];
+    }
 
     public function category(): BelongsTo
     {
---
+++ config/elasticsearch.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'host' => env('ELASTICSEARCH_HOST', 'http://rewards-elastic:9200'),
+];
